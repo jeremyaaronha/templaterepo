dfdab011e5e3be9ebc974f6c72873ff40be977c3
jeremyaaronha <jeremyaaronha@gmail.com>
2024-09-20 15:35:07 -0300

    fix order in server.js

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
+
+/* ***********************
+ * "View Engine and Templates"
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+
 /* ***********************
  * Routes
  *************************/
@@ -21,12 +30,7 @@ app.get("/", function(req, res){
   res.render("index", {title: "Home"})
 })
 
-/* ***********************
- * "View Engine and Templates"
- *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+
 
 /* ***********************
  * Local Server Information
