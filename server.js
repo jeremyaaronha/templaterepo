160e11b4f26f761fbe7f6927464621876d1c2453
jeremyaaronha <jeremyaaronha@gmail.com>
2024-10-27 05:26:23 -0300

    try fix port render

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.use(async (err, req, res, next) => {
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
+const port = process.env.PORT || 5500;
 const host = process.env.HOST
 
 /* ***********************
